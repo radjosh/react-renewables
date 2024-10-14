ea5ec71f236ccda01253725bc6867b2eea5b884b
Joshua M Smith <radjosh@gmail.com>
2024-10-14 12:43:03 +0000

    wip: composite key for each result

--- components/App/App.js
+++ components/App/App.js
@@ -30,8 +30,10 @@ function App() {
       gap: 4,
       }} 
     >
-      { Object.values(results)?.map(result => (
-        <Paper elevation={3}>
+      { Object.values(results)?.map(result => {
+        const compositeKey = `${result.Country}-${result.Year}-${result.EnergyType}` 
+        return (
+        <Paper key={compositeKey} elevation={3}>
           <Box sx={{ 
             borderRadius: 8,
             flex: "1 1 calc(33.33% - 16px)",
@@ -41,66 +43,13 @@ function App() {
           }} >
           <Typography variant="h6" sx={{ textAlign: 'left' }}>{ result.Country }{" "}{ result.Year }{" "}{ result.EnergyType }</Typography>
           { Object.entries(result).map(([k, v]) => 
-          <Typography key={crypto.randomUUID} sx={{ textAlign: 'left', fontSize: 'xx-small'}}>
-            {k}: {v}
-          </Typography>
+            <Typography key={crypto.randomUUID} sx={{ textAlign: 'left', fontSize: 'xx-small'}}>
+              {k}: {v}
+            </Typography>
           )}
-          {/* <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>ProductionGWh: { result.ProductionGWh }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>InstalledCapacityMW: { result.InstalledCapacityMW }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>InvestmentsUSD: { result.InvestmentsUSD }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>Population: { result.Population }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>GDP: { result.GDP }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>EnergyConsumption: { result.EnergyConsumption }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>EnergyExports: { result.EnergyExports }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>EnergyImports: { result.EnergyImports }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>CO2Emissions: { result.CO2Emissions }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>RenewableEnergyJobs: { result.RenewableEnergyJobs }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>GovernmentPolicies: { result.GovernmentPolicies }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>RnDExpenditure: { result.RnDExpenditure }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>RenewableEnergyTargets: { result.RenewableEnergyTargets }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>AverageAnnualTemperature: { result.AverageAnnualTemperature }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>AnnualRainfall: { result.AnnualRainfall }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>SolarIrradiance: { result.SolarIrradiance }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>WindSpeed: { result.WindSpeed }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>HydroPotential: { result.HydroPotential }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>GeothermalPotential: { result.GeothermalPotential }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>BiomassAvailability: { result.BiomassAvailability }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>EnergyStorageCapacity: { result.EnergyStorageCapacity }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>GridIntegrationCapability: { result.GridIntegrationCapability }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>ElectricityPrices: { result.ElectricityPrices }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>EnergySubsidies: { result.EnergySubsidies }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>InternationalAidForRenewables: { result.InternationalAidForRenewables }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>PublicAwareness: { result.PublicAwareness }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>EnergyEfficiencyPrograms: { result.EnergyEfficiencyPrograms }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>UrbanizationRate: { result.UrbanizationRate }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>IndustrializationRate: { result.IndustrializationRate }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>EnergyMarketLiberalization: { result.EnergyMarketLiberalization }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>RenewableEnergyPatents: { result.RenewableEnergyPatents }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>EducationalLevel: { result.EducationalLevel }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>TechnologyTransferAgreements: { result.TechnologyTransferAgreements }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>RenewableEnergyEducationPrograms: { result.RenewableEnergyEducationPrograms }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>LocalManufacturingCapacity: { result.LocalManufacturingCapacity }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>ImportTariffsOnEnergyEquipment: { result.ImportTariffsOnEnergyEquipment }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>ExportIncentivesForEnergyEquipment: { result.ExportIncentivesForEnergyEquipment }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>NaturalDisasters: { result.NaturalDisasters }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>PoliticalStability: { result.PoliticalStability }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>CorruptionPerceptionIndex: { result.CorruptionPerceptionIndex }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>RegulatoryQuality: { result.RegulatoryQuality }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>RuleOfLaw: { result.RuleOfLaw }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>ControlOfCorruption: { result.ControlOfCorruption }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>EconomicFreedomIndex: { result.EconomicFreedomIndex }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>EaseOFDoingBusiness: { result.EaseOFDoingBusiness }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>InnovationIndex: { result.InnovationIndex }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>NumberOfResearchInstitutions: { result.NumberOfResearchInstitutions }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>NumberOFRenewableEnergyConferences: { result.NumberOFRenewableEnergyConferences }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>NumberOfRenewableEnergyPublications: { result.NumberOfRenewableEnergyPublications }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>EnergySectorWorkforce: { result.EnergySectorWorkforce }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>ProportionOfEnergyFromRenewables: { result.ProportionOfEnergyFromRenewables }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>PublicPrivatePartnershipsInEnergy: { result.PublicPrivatePartnershipsInEnergy }</Typography>
-          <Typography sx={{ textAlign: 'left', fontSize: 'xx-small'}}>RegionalRenewableEnergyCooperation: { result.RegionalRenewableEnergyCooperation }</Typography> */}
           </Box>
         </Paper>
-     ))}
+      )})}
     </Box>
     </Container>
   )
